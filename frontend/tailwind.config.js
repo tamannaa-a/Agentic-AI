f6c1b15a4835d15b832a478668befd6f7d32d3d5
Tamanna Vaikkath <vaikkathtamanna@gmail.com>
2025-11-10 22:49:46 +0530

    tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,20 @@
 export default {
+  darkMode: 'class',
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [require('@tailwindcss/forms')],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui']
+      },
+      colors: {
+        neon: {
+          50: '#e8f0ff',
+          100: '#cfe0ff',
+          300: '#4fd1ff',
+          500: '#00e5ff'
+        }
+      }
+    }
+  },
+  plugins: [],
 }
